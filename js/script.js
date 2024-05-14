343ee4992d5613ef1db93464a181e18ec96574dc
Dzakw <iqbaldzakwani@gmail.com>
2024-05-14 15:30:15 +0800

    check geolocation

--- js/script.js
+++ js/script.js
@@ -101,16 +101,16 @@ function showPosition(position) {
 function showError(error) {
   switch(error.code) {
     case error.PERMISSION_DENIED:
-      x.innerHTML = "User denied the request for Geolocation."
+      x.innerHTML = "User denied the request for Geolocation.";
       break;
     case error.POSITION_UNAVAILABLE:
-      x.innerHTML = "Location information is unavailable."
+      x.innerHTML = "Location information is unavailable.";
       break;
     case error.TIMEOUT:
-      x.innerHTML = "The request to get user location timed out."
+      x.innerHTML = "The request to get user location timed out.";
       break;
     case error.UNKNOWN_ERROR:
-      x.innerHTML = "An unknown error occurred."
+      x.innerHTML = "An unknown error occurred.";
       break;
   }
 }
@@ -119,11 +119,12 @@ const xx = document.getElementById("demo");
 
 function getLocations() {
   if (navigator.geolocation) {
-    navigator.geolocation.watchPosition(showPositions);
+    navigator.geolocation.watchPosition(showPositions, showError);
   } else {
     xx.innerHTML = "Geolocation is not supported by this browser.";
   }
 }
+
 function showPositions(position) {
   xx.innerHTML = "Latitude: " + position.coords.latitude +
   "<br>Longitude: " + position.coords.longitude;
