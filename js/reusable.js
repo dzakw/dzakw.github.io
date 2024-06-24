fcd440b2b1b6261ad10f4e4bbcd01caeb8953d83
Dzakw <iqbaldzakwani@gmail.com>
2024-06-24 10:35:45 +0800

    navbar responsive

--- js/reusable.js
+++ js/reusable.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     const hamburger = document.querySelector(".hamburger");
     const navbarCollapse = document.querySelector(".navbar-collapse");
-    const dropdown = document.querySelector(".dropdown .dropbtn");
 
     hamburger.addEventListener("click", function(event) {
         navbarCollapse.classList.toggle("show");
@@ -14,23 +13,20 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
-    const dropdownToggles = document.querySelectorAll(".dropdown-toggle");
+});
 
-    dropdownToggles.forEach(dropdownToggle => {
-        dropdownToggle.addEventListener("click", function(event) {
-            const dropdown = this.closest(".dropdown");
-            dropdown.classList.toggle("show");
-            event.stopPropagation();
-        });
+document.addEventListener("DOMContentLoaded", function() {
+    const accordionInner = document.querySelector(".accordion-inner");
+    const dropdownContent = document.querySelector(".navbar-collapse .dropdown-content");
+
+    accordionInner.addEventListener("click", function(event) {
+        dropdownContent.classList.toggle("show");
+        event.stopPropagation();
     });
 
-    // Close dropdown when clicking outside
     document.addEventListener("click", function(event) {
-        dropdownToggles.forEach(dropdownToggle => {
-            const dropdown = dropdownToggle.closest(".dropdown");
-            if (!dropdown.contains(event.target)) {
-                dropdown.classList.remove("show");
-            }
-        });
+        if (!dropdownContent.contains(event.target) && !accordionInner.contains(event.target)) {
+            dropdownContent.classList.remove("show");
+        }
     });
 });
